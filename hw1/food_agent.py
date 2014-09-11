fe5212ec0e82cc3d5470ce585a225ff59ff736d5
Sean <sean.papay@gmail.com>
2014-09-10 19:22:07 -0700

    added heuristics n stuff

--- hw1/food_agent.py
+++ hw1/food_agent.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 if len(sys.argv) != 3 :
 	print "Usage : python food_agent.py file_name heuristic"
@@ -24,5 +25,30 @@ def printMaze(m) :
 			s += el
 		print s
 		
+def findChar(ch, m) :
+	r = 0
+	for row in m:
+		c = 0
+		for el in row:
+			if el == ch:
+				return r, c
+			c += 1
+		r += 1
 
-printMaze(readMaze(textFile))
+heur = sys.argv[2]
+
+if heur == "euclidian" :
+	def heuristic(player, goal) :
+		dx = player[0] - goal[0]
+		dy = player[1] - goal[1]
+		return math.sqrt(dx*dx + dy*dy)
+elif heur == "manhattan" :
+		def heuristic(player, goal) :
+			dx = player[0] - goal[0]
+			dy = player[1] - goal[1]
+			return abs(dx) + abs(dy);
+
+m = readMaze(textFile)
+playerLoc = findChar('@', m)
+goalLoc = findChar('%', m)
+print heuristic((0, 0), (5, 12))
