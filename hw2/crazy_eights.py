634c359b13041c8518f0470226744658e6f95f4a
Sean <sean.papay@gmail.com>
2014-09-28 14:31:42 -0700

    setting up methods

---
+++ hw2/crazy_eights.py
@@ -0,0 +1,21 @@
+def shuffle_deck():
+	
+def gen_initial_state():
+
+def gen_moves(partial_state):
+
+def make_move(move, state):
+	
+def undo_move(state):
+
+def is_game_over(state):
+
+def get_winner(state):
+
+
+
+class CrazyEight:
+	def move(self, partial_state):
+	
+	def move_perfect_knowlege(self, state):
+	
