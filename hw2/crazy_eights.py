b5a026a27fb9159906425b1b162e00c2e48f2c1c
rlichtig <rlichtig@gmail.com>
2014-09-28 15:15:38 -0700

    Ryan's work
    Added in get_suit(card_number), get_rank(card_number), and created a
    randomly generated deck in gen_initial_state()

--- hw2/crazy_eights.py
+++ hw2/crazy_eights.py
@@ -1,21 +1,41 @@
-def shuffle_deck():
+import random
+    
+def get_suit(card_number):
+	#Suit is an integer among 0,1,2,3
+	# 0 - Spades
+	# 1 - Hearts
+	# 2 - Diamonds
+	# 3 - Clubs
+	if card_number >= 0 and card_number <= 12
+		return 0
+	if card_number >= 13 and card_number <= 25
+		return 1
+	if card_number >= 26 and card_number <= 38
+		return 2
+	if card_number >= 39 and card_number <= 51
+		return 3
+	return -1
+
+def get_rank(card_number):
+	return (card_number % 13) + 1
 	
 def gen_initial_state():
-
+    deck = random.sample(range(52),52)
+	return
 def gen_moves(partial_state):
-
+    return
 def make_move(move, state):
-	
+    return
 def undo_move(state):
-
+    return
 def is_game_over(state):
-
+    return
 def get_winner(state):
-
+    return
 
 
 class CrazyEight:
 	def move(self, partial_state):
-	
+	    return
 	def move_perfect_knowlege(self, state):
-	
+	    return
