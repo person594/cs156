3a3b90664cdaa26833b0058900e072783b6a99b1
rlichtig <rlichtig@gmail.com>
2014-09-28 15:55:35 -0700

    implementation of gen_initial_state
    please lord actually work

--- hw2/crazy_eights.py
+++ hw2/crazy_eights.py
@@ -4,27 +4,25 @@ import random
 player_number = 0
     
 def get_suit(card_number):
-	#Suit is an integer among 0,1,2,3
-	# 0 - Spades
-	# 1 - Hearts
-	# 2 - Diamonds
-	# 3 - Clubs
-	if card_number >= 0 and card_number <= 12
-		return 0
-	if card_number >= 13 and card_number <= 25
-		return 1
-	if card_number >= 26 and card_number <= 38
-		return 2
-	if card_number >= 39 and card_number <= 51
-		return 3
-	return -1
+	return card_number / 13
 
 def get_rank(card_number):
 	return (card_number % 13) + 1
 	
 def gen_initial_state():
     deck = random.sample(range(52),52)
-	return
+    p0_hand[]
+    p1_hand[]
+    count = 0
+    while (count < 8)
+    	p0_hand.append(deck.pop())
+    	p1_hand.append(deck.pop())
+    	count += 1
+    face_up_card = deck.pop()
+    initial_move = (1,face_up_card,get_suit(face_up_card),0)
+    initial_partial_state = (face_up_card,get_suit(face_up_card),p0_hand,initial_move)
+    intiail_state = (deck,p1_hand,initial_partial_state)
+	return initial_state
 
 def gen_moves(partial_state):
     move_list = {}
