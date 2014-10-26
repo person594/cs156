bc06d57b37890a51dcd94b3c1affaa6885a6cd3b
Sean <sean.papay@gmail.com>
2014-10-26 16:41:02 -0700

    changes

--- hw3/csp_solver.py
+++ hw3/csp_solver.py
@@ -1,22 +1,22 @@
 import operator
+import sys
 
-#returns a tuple ([list_of variables], [list_of_relations], domain_upper_bound)
-#tuple will actually probably be ([(variable, domain)], [relations])
-#just kidding, it will really be ({variable:domain}, [relations])
+#returns a tuple ({variable:domain}, [relations])
 def parse_file(filename):
 	varList = []
 	relList = []
 	upperBound = 0
-	file = open('filename', 'r')
+	file = open(filename, 'r')
 	for line in file:
 		parseTuple = line.split()
-		terms = [parseTuple[0], parsetuple[2]]
+		terms = [parseTuple[0], parseTuple[2]]
 		#Checks if the first character in the variables are digits
 		second_term_is_var = True
 		if terms[0][0].isdigit():
-			#this is an error, yell at the user
-		if newVars[1][0].isdigit():
-			terms[0] = int(terms[0])
+			exit()
+		#this is an error, yell at the user
+		if terms[1][0].isdigit():
+			terms[1] = int(terms[1])
 			second_term_is_var = False
 		#Checks list of variables for repeats
 		if not terms[0] in varList:
@@ -24,21 +24,27 @@ def parse_file(filename):
 		if second_term_is_var and not terms[1] in varList:
 			varList.append(terms[1])
 		#Adds relation to the relation list
-		relation = parsedTuple[1]
+		relation = parseTuple[1]
 		if relation == 'ne': relation = operator.ne
 		elif relation == 'eq': relation = operator.eq
 		elif relation == 'lt': relation = operator.lt
 		elif relation == 'gt': relation = operator.gt
 		relList.append((terms[0], relation, terms[1]))
 		#Finds new upperbound
-		if not second_term_is_var
+		if not second_term_is_var:
 			if terms[1] > upperBound: upperBound = terms[1]
 	varDic = {}
+	upperBound = max(upperBound, len(varList)+1)
 	for var in varList:
-		tmp = {var : range(0, upperBound)}
-		varDic.append(tmp)
+		varDic[var] = set(range(0, upperBound))
 	parsed = (varDic, relList)
+	file.close()
 	return parsed
+
+
+def check_consistency(var, val, assignments, relations):
+	
+
 
 def flip_relation(rel):
 	if (rel[1] == operator.lt):
@@ -87,7 +93,7 @@ def Backtracking-Search(csp):
 def Backtrack(assignment, csp):
 	variable = None
 	for var in csp[0]:
-		if not var[0] in assignment:
+		if not var in assignment:
 			#MRV heuristic
 			if (variable is None) or (len(var[1]) < len(variable[1])):
 				variable = var
@@ -97,8 +103,8 @@ def Backtrack(assignment, csp):
 	
 	
 		
-   for each value in Order-Domain-Values(var, assignment, csp) do
-        if value is consistent with assignment then
+   for value in Order-Domain-Values(var, assignment, csp):
+        if value is consistent with assignment:
             add {var= value} to assignment
             inferences := INFERENCE(csp, var, value) 
                 //this might do AC-3 or the like
@@ -109,3 +115,9 @@ def Backtrack(assignment, csp):
                     return result
         remove {var = value} and inferences from assignment
    return failure
+
+
+csp = parse_file(sys.argv[1])
+for var in csp[0]:
+	print var + " e " + str(csp[0][var])
+
